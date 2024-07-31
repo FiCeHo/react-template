1632150512711380ab744fa941f8d3d1d5389d0d
FiCeHo <filip.ceremis@sarria.salesians.cat>
2024-07-31 13:25:40 +0200

    build: cagada2, putadon

--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,4 @@ const config = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config);;;;;
+module.exports = createJestConfig(config)
